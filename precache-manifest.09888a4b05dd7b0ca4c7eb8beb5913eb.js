8e2168cbbd0347fd1c42e396cff7d0c6b010460d
petra-vozarova <61162312+petra-vozarova@users.noreply.github.com>
2020-07-22 12:26:23 +0100

    Updates

---
+++ precache-manifest.09888a4b05dd7b0ca4c7eb8beb5913eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f464a8b5e01b768ca440244e09a1547",
+    "url": "/jukebox/index.html"
+  },
+  {
+    "revision": "be3c2ff0aeebe0803bbc",
+    "url": "/jukebox/static/css/main.10fcc7ae.chunk.css"
+  },
+  {
+    "revision": "d6b9e56e68c6b7feae10",
+    "url": "/jukebox/static/js/2.ffaf70b8.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/jukebox/static/js/2.ffaf70b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be3c2ff0aeebe0803bbc",
+    "url": "/jukebox/static/js/main.d772e959.chunk.js"
+  },
+  {
+    "revision": "f4356bc72a5bbaf133f6",
+    "url": "/jukebox/static/js/runtime-main.c35e8540.js"
+  }
+]);
